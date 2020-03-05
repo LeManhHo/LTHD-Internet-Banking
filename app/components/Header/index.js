60237158a3aff877038bee5dd1749f7dfb549745
dungntp <dungnguyen@lecle.co.kr>
2020-03-05 19:24:09 +0700

    add top bar menu

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,27 +1,61 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Badge,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
+import InputIcon from '@material-ui/icons/Input';
 
-import A from './A';
-import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
-import messages from './messages';
+const logoImg = require('../../images/logo.png');
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: 'bold',
+  },
+  logo: {
+    height: 50,
+    width: 70,
+    marginRight: 15,
+  },
+  logoImg: {
+    width: '100%',
+  },
+}));
 
 function Header() {
+  const classes = useStyles();
   return (
     <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
+      <AppBar position="static">
+        <Toolbar>
+          <RouterLink to="/" className={classes.logo}>
+            <img className={classes.logoImg} alt="Logo" src={logoImg} />
+          </RouterLink>
+          <Typography variant="h6" className={classes.title}>
+            TIGER BANK
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={2} color="primary" variant="dot">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton className={classes.signOutButton} color="inherit">
+            <InputIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
